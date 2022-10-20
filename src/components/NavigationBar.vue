e409062012e34f35c7b947c810075bd6662094d2
Mathis-CHALANDARD <mathischalandard@gmail.com>
2022-10-20 15:54:38 +0200

    Modification de fichiers

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="NavigationMenu">
+  </div>
+</template>
+
+<style scoped>
+.NavigationMenu{
+    width: 100%;
+    min-height: 10vh;    
+    max-height: 10vh;
+    background-color: hsla(160, 100%, 37%, 0.2);
+}
+
+@media (width: 100%) {
+  .NavigationMenu{
+    width: 100%;
+    min-height: 10vh;
+    max-height: 10vh;
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+</style>
