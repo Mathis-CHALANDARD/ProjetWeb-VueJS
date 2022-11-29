aec2c28d4292d7f0b9a205bb991bceb61c39fd20
Mathis-CHALANDARD <mathischalandard@gmail.com>
2022-11-29 12:25:49 +0100

    Avancement

---
+++ src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<script>
+//barre de navigation
+import NavBarBtn from "./NavBarBtn.vue";
+export default {
+    props: [],
+    data() {
+        return {};
+    },
+    methods: {
+        goHome() {
+        }
+    },
+    mounted() { },
+    components: { NavBarBtn }
+};
+</script>
+
+<template>
+	<div class="NavBar">
+        <div class="SiteName">
+            <img class="Logo" src="../assets/Icone_chat.png">
+            <router-link to="/" class="SiteName"> CatCatCatCat</router-link>
+        </div>
+        <div class="btns">
+            <NavBarBtn icon = "search" route = "/"></NavBarBtn>
+            <NavBarBtn icon = "login" route = "/"></NavBarBtn>
+            <NavBarBtn icon = "menu" route = "/"></NavBarBtn>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.NavBar{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    min-height: 100px;
+    background-color: #42b883;
+}
+.Logo{
+    height: 50px;
+    width: 50px;
+    padding-left: 2rem;
+    color: white;
+}
+.SiteName{
+    color: lightgray;
+    font-size: 50px;
+}
+.btns{
+    display: flex;
+    min-width: 200px;
+    justify-content: space-around;
+    padding-top: 1rem;
+    padding-right: 2rem;
+}
+
+</style>
---
+++ src/components/NavBarBtn.vue
@@ -0,0 +1,47 @@
+<script>
+//barre de navigation
+export default {
+	props: {
+        icon:{
+            type : String,
+            default : 'cancel'
+        },
+
+        routeName:{
+            type : String,
+            default : '/'
+        }
+    },
+	data() {
+		return {};
+	},
+
+	methods: {
+        goTo(){
+            this.$router.push({
+                name : this.routeName
+            });
+        }
+    },
+
+	mounted() {},
+};
+</script>
+
+<template>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+    <router-link :to="this.routeName" class="btnColor"> 
+        <span class="material-symbols-outlined">
+        {{icon}}
+        </span>
+    </router-link>
+        
+</template>
+
+<style scoped>
+
+    .btnColor {
+        color: #fff;
+    }
+
+</style>
